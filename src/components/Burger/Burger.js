5b7e1e7383b53dbda0d6db3969b27bd7e4d44c78
joeface71 <joefacenda@yahoo.com>
2019-03-26 17:57:32 -0400

    Build Controls Added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,19 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients) //{'salad', 'bacon', 'cheese', 'meat'}
+  let transformedIngredients = Object.keys(props.ingredients) //{'salad', 'bacon', 'cheese', 'meat'}
     .map(igKey => {
       return [...Array(props.ingredients[igKey])] //(4) [Array(1), Array(1), Array(2), Array(2)] -- all elements undefined
       .map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />; //keys assigned eg salad0, bacon0, cheese0, cheese1, etc
       }); 
-    });
-    console.log(transformedIngredients)
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    },[]);
+  if (transformedIngredients.length === 0) {
+     transformedIngredients = <p>Please Add Ingredients</p>
+  }
       
   return (
     <div className={classes.Burger}>
