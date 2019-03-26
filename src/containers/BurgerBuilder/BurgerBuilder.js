4fdb4ba9c0fabab5cd3a324b007b4b211fbecf7b
joeface71 <joefacenda@yahoo.com>
2019-03-26 18:12:12 -0400

    State Adjustment

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,29 +1,41 @@
-import React, { Component } from 'react';
-import Aux from '../../hoc/Auxillary';
-import Burger from '../../components/Burger/Burger'
-import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxillary";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
-    // constructor(props) {  not needed with latest react
-    //     super(props);
-    //     this.state = {...this};
-    // }
-    state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0, 
-            meat: 0
-        }
-    };
-    render() {
-        return (
-            <Aux>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
-            </Aux>
-        );
+  // constructor(props) {  not needed with latest react
+  //     super(props);
+  //     this.state = {...this};
+  // }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
+  };
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+  };
+
+  removeIngredientHandler = type => {};
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder;
